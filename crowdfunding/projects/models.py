ab86a09a79cbb03bc8dc932efe772511e2bdaed9
shay <shay.zimmerle@gmail.com>
2024-01-13 21:51:53 +0800

    handling model relationships update

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -15,4 +15,9 @@ class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
-    
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
