8490d5be9b7cecccabe944a06fbd7c4b2ff2306e
shay <shay.zimmerle@gmail.com>
2024-01-14 17:25:31 +0800

    users update

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -9,7 +10,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -20,4 +25,8 @@ class Pledge(models.Model):
         on_delete=models.CASCADE,
         related_name='pledges'
     )
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
